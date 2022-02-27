bfeb08c74c27c351661e8d91c9ca81b5ad587920
pklgn <petechka_petrov_201@mail.ru>
2022-02-27 20:05:11 +0300

    improve second and third tasks

--- lab1/multmatrix/multmatrix.cpp
+++ lab1/multmatrix/multmatrix.cpp
@@ -8,63 +8,58 @@ typedef double Matrix3x3[SQUARE_MATRIX_SIZE][SQUARE_MATRIX_SIZE];
 
 bool ReadMatrix3x3(const std::string& fileName, Matrix3x3& result)
 {
-	bool status = true;
 	std::ifstream inputFile(fileName);
 	if (!inputFile.is_open())
 	{
 		std::cout << "Failed to open " << fileName << " for reading" << std::endl;
-		status = false;
-		//return и убрать else
+		return false;
 	}
-	else
+	for (int currRow = 0; currRow < SQUARE_MATRIX_SIZE; currRow++)
 	{
-		for (int currRow = 0; currRow < SQUARE_MATRIX_SIZE && status; currRow++)
+		for (int currColumn = 0; currColumn < SQUARE_MATRIX_SIZE; currColumn++)
 		{
-			for (int currColumn = 0; currColumn < SQUARE_MATRIX_SIZE && status; currColumn++)
+			if (inputFile.bad() || inputFile.eof())
 			{
-				double d;
-				if (inputFile.bad() || inputFile.eof())
-				{
-					std::cout << "Cannot read matrix from file" << std::endl;
-					status = false;
-					break;
-					//return false
-				}
-				try
-				{
-					//определять значение бита ошибки и убрать try catch
-
-					std::string matrixElement;
-					inputFile >> matrixElement;
-					d = std::stod(matrixElement);
-					result[currRow][currColumn] = d;
-				}
-				catch (...)
-				{
-					std::cout << "Error while reading an element" << std::endl;
-
-					exit(EXIT_FAILURE);
-					//не использовать exit 
-
-					
-				}
+				std::cout << "Cannot read matrix from file" << std::endl;
+				return false;
 			}
+			
+			//определять значение бита ошибки и убрать try catch
+			double elt;
+			inputFile >> elt;
+			if (inputFile.fail())
+			{
+				std::cout << "Couldn't read a double from the file." << std::endl;
+				return false;
+			}
+			result[currRow][currColumn] = elt;
 		}
 	}
 
-	return status;
+	return true;
 }
 //разделить умножение и печать + убрать инициализцию и принимать по const &
-void MultiplyMatrix3x3(Matrix3x3 aMatrix, Matrix3x3 bMatrix)
+void MultiplyMatrix3x3(const Matrix3x3& aMatrix, const Matrix3x3& bMatrix, Matrix3x3& result)
 {
-	Matrix3x3 result = { {0, 0, 0}, {0, 0, 0}, {0, 0, 0} };
-
 	for (int row = 0; row < SQUARE_MATRIX_SIZE; row++) {
 		for (int column = 0; column < SQUARE_MATRIX_SIZE; column++) {
 			for (int inter = 0; inter < SQUARE_MATRIX_SIZE; inter++) {
 				result[row][column] += aMatrix[row][inter] * bMatrix[inter][column];
 			}
-			std::cout << std::fixed << std::setprecision(3) << result[row][column] << "  ";
+			//std::cout << std::fixed << std::setprecision(3) << result[row][column] << "  ";
+		}
+		//std::cout << "\n";
+	}
+	//std::cout << "\n";
+}
+
+void PrintMatrix3x3(const Matrix3x3 matrix)
+{
+	for (int row = 0; row < SQUARE_MATRIX_SIZE; row++)
+	{
+		for (int column = 0; column < SQUARE_MATRIX_SIZE; column++)
+		{
+			std::cout << std::fixed << std::setprecision(3) << matrix[row][column] << "  ";
 		}
 		std::cout << "\n";
 	}
@@ -73,9 +68,7 @@ void MultiplyMatrix3x3(Matrix3x3 aMatrix, Matrix3x3 bMatrix)
 
 int main(int argc, char* argv[])
 {
-	//перенести к функции
-	Matrix3x3 leftMatrix;
-	Matrix3x3 rightMatrix;
+
 
 	// добавить функцию парсинга 
 	if (argc != 3)
@@ -89,12 +82,15 @@ int main(int argc, char* argv[])
 	std::string leftMatrixFileName = argv[1];
 	std::string rightMatrixFileName = argv[2];
 
-	if (!(ReadMatrix3x3(leftMatrixFileName, leftMatrix) && ReadMatrix3x3(rightMatrixFileName, rightMatrix)))
+	Matrix3x3 aMat;
+	Matrix3x3 bMat;
+	if (!(ReadMatrix3x3(leftMatrixFileName, aMat) && ReadMatrix3x3(rightMatrixFileName, bMat)))
 	{
 		return 1;
 	}
 	
-	MultiplyMatrix3x3(leftMatrix, rightMatrix);
+	Matrix3x3 rMat;
+	MultiplyMatrix3x3(aMat, bMat, rMat);
 
 	return 0;
 }
