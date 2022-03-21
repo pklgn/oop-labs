3879ed1be6f2713a7bcf084446bc9e00efe7d82b
pklgn <petechka_petrov_201@mail.ru>
2022-03-21 04:22:19 +0300

    [lab2-1-vector] add tests

---
+++ lab2/vector/double_vector.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+#include <vector>
+
+bool ReadNumbers(std::istream& inputStream, std::vector<double>& outputVec);
+
+void PrintVector(std::vector<double>& vec, std::ostream& output);
+
+std::vector<double> MultiplyNegativeByMinMaxOfVector(std::vector<double>& vec);
--- lab2/vector/main.cpp
+++ lab2/vector/main.cpp
@@ -3,12 +3,7 @@
 #include <fstream>
 #include <algorithm>
 #include <iomanip>
-
-constexpr short FLOAT_PRECISION = 3;
-
-bool ReadNumbers(std::istream& inputStream, std::vector<double>& outputVec);
-void PrintVector(std::vector<double>& vec);
-std::vector<double> MultiplyNegativeByMinMaxOfVector(std::vector<double>& vec);
+#include "double_vector.h"
 
 int main(int argc, char* argv[])
 {
@@ -20,48 +15,7 @@ int main(int argc, char* argv[])
 	}
 
 	std::vector<double> processedNumbers = MultiplyNegativeByMinMaxOfVector(numbers);
-	PrintVector(processedNumbers);
+	PrintVector(processedNumbers, std::cout);
 
 	return 0;
-}
-
-bool ReadNumbers(std::istream& inputStream, std::vector<double>& outputVec)
-{
-	std::copy(std::istream_iterator<double>(inputStream), std::istream_iterator<double>(), 
-		std::back_inserter(outputVec));
-
-	if (inputStream.fail() && !inputStream.eof())
-	{
-		std::cout << "Invalid number value was found\n";
-
-		return false;
-	}
-
-	return true;
-}
-
-void PrintVector(std::vector<double>& vec)
-{
-	std::cout << std::fixed << std::setprecision(FLOAT_PRECISION);
-	std::copy(vec.begin(), vec.end(), std::ostream_iterator<double>(std::cout, " "));
-}
-
-std::vector<double> MultiplyNegativeByMinMaxOfVector(std::vector<double>& vec)
-{
-	auto bounds = std::minmax_element(vec.begin(), vec.end());
-
-	std::vector<double> resultVec;
-	auto multiply = [&](double element) {
-		if (element < 0)
-		{
-			element *= *bounds.first * *bounds.second;
-		}
-		
-		resultVec.push_back(element);
-	};
-
-	std::for_each(vec.begin(), vec.end(), multiply);
-	std::sort(resultVec.begin(), resultVec.end());
-
-	return resultVec;
 }
