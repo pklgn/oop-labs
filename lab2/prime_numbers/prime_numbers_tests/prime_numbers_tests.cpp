e6185e2f98517ac194281abc3bc295355a8d2a35
pklgn <petechka_petrov_201@mail.ru>
2022-03-28 03:18:41 +0300

    [lab2-4-prime_numbers] add tests

---
+++ lab2/prime_numbers/prime_numbers_tests/prime_numbers_tests.cpp
@@ -0,0 +1,42 @@
+#define CATCH_CONFIG_MAIN
+#include "../../../catch2/catch.hpp"
+#include "../prime_numbers_lib/prime_number_lib.h"
+
+SCENARIO("Giving to the function valid values")
+{
+	WHEN("Input value is negative")
+	{
+		const std::set<int> result = GeneratePrimeNumbersSet(-2);
+		THEN("The result set will be empty")
+		{
+			REQUIRE(result.empty());
+		}
+	}
+
+	WHEN("Input value is less than the minimum prime number which equals 2")
+	{
+		const std::set<int> result = GeneratePrimeNumbersSet(1);
+		THEN("The result set will be empty")
+		{
+			REQUIRE(result.empty());
+		}
+	}
+
+	WHEN("Input value is small enough to calculate it by myself")
+	{
+		const std::set<int> set = GeneratePrimeNumbersSet(100);
+		THEN("Size of the result for upperBound = 100 will be equal to 25")
+		{
+			REQUIRE(set.size() == 25);
+		}
+	}
+
+	WHEN("Input value is the maximum")
+	{
+		const std::set<int> set = GeneratePrimeNumbersSet(100000000);
+		THEN("Size of the result for upperBound = 100000000 will be equal to 5761455")
+		{
+			REQUIRE(set.size() == 5761455);
+		}
+	}
+}
