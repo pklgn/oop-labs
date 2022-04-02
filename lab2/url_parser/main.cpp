0d11729845d873d6f1e34b2c26f6cc39309d87f2
pklgn <petechka_petrov_201@mail.ru>
2022-04-03 01:22:19 +0300

    [lab2-5-url_parser] add GetDefaultPort function

--- lab2/url_parser/main.cpp
+++ lab2/url_parser/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <regex>
+#include <vector>
+#include <map>
 
 enum class Protocol
 {
@@ -8,9 +11,36 @@ enum class Protocol
 	FTP
 };
 
+constexpr unsigned short FTP_PORT_DEFAULT = 21;
+constexpr unsigned short HTTP_PORT_DEFAULT = 80;
+constexpr unsigned short HTTPS_PORT_DEFAULT = 443;
+constexpr unsigned short NO_PORT_DEFAULT = 0;
+
 bool ParseURL(std::string const& url, Protocol& protocol, int& port, std::string& host, std::string& document)
 {
 	bool isCorrect = true;
 
+	return isCorrect;
+}
 
+unsigned short GetDefaultPort(Protocol& protocol)
+{
+	switch (protocol)
+	{
+		case Protocol::FTP:
+			return FTP_PORT_DEFAULT;
+		case Protocol::HTTP:
+			return HTTP_PORT_DEFAULT;
+		case Protocol::HTTPS:
+			return HTTPS_PORT_DEFAULT;
+	}
+
+	std::cout << "Can't match protocol and default port\n";
+
+	return NO_PORT_DEFAULT;
+}
+
+int main()
+{
+	return 0;
 }
