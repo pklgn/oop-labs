3019eaf671a6df230572befd8c9ca59467129b39
Pavel Ermakov <73781985+pklgn@users.noreply.github.com>
2022-05-16 02:32:28 +0300

    [lab4-1-Shapes] small improvements

--- lab4/Shapes/main.cpp
+++ lab4/Shapes/main.cpp
@@ -10,23 +10,31 @@ int main()
 
 	while (!std::cin.eof())
 	{
-		++counter;
 		if (!shapesController.ReadShape(std::cin) && !std::cin.eof())
 		{
-			std::cout << "Cannot read shape in " << counter << " position\n";
+			std::cout << "Cannot read shape in " << counter + 1 << " position\n";
+		}
+		else if (!std::cin.eof())
+		{
+			++counter;
 		}
 	}
 
-	std::cout << "Read shapes:\n";
-	shapesController.PrintShapesInfo(std::cout);
+	if (counter > 0)
+	{
+		std::cout << "Read shapes:\n";
+		shapesController.PrintShapesInfo(std::cout);
 
-	auto maxAreaShapeIt = *shapesController.FindMaxAreaShape();
-	auto minPerimeterShapeIt = *shapesController.FindMinPerimeterShape();
+		auto maxAreaShapeIt = *shapesController.FindMaxAreaShape();
+		auto minPerimeterShapeIt = *shapesController.FindMinPerimeterShape();
 
-	std::cout << "Max area shape\n" << maxAreaShapeIt->ToString() << std::endl;
-	std::cout << "Min perimeter shape\n" << minPerimeterShapeIt->ToString() << std::endl;
+		std::cout << "Max area shape\n"
+				  << maxAreaShapeIt->ToString() << std::endl;
+		std::cout << "Min perimeter shape\n"
+				  << minPerimeterShapeIt->ToString() << std::endl;
 
-	shapesController.DrawShapes(800, 600, "Shapes");
+		shapesController.DrawShapes(800, 600, "Shapes");
+	}
 
 	return 0;
 }
