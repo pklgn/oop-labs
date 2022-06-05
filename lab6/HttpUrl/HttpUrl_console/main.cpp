eda6c592f309f3ec4ad7ee12838fd57d82455728
Pavel Ermakov <73781985+pklgn@users.noreply.github.com>
2022-06-05 22:08:13 +0300

    [lab6-1-HttpUrl] add console app

---
+++ lab6/HttpUrl/HttpUrl_console/main.cpp
@@ -0,0 +1,34 @@
+#include "../HttpUrl.h"
+
+
+void ProcessURLParsing(std::istream& inputStream, std::ostream& outputStream);
+
+int main()
+{
+	ProcessURLParsing(std::cin, std::cout);
+
+	return 0;
+}
+
+void ProcessURLParsing(std::istream& inputStream, std::ostream& outputStream)
+{
+	bool isCorrect;
+	std::string url;
+	while (std::getline(inputStream, url))
+	{
+		try
+		{
+			HttpUrl inputUrl = HttpUrl(url);
+			outputStream << "Protocol: " << (inputUrl.GetProtocol() == HttpUrl::Protocol::HTTP ? "http" : "https") << std::endl
+						 << "Domain:   " << inputUrl.GetDomain() << std::endl
+						 << "Port:     " << inputUrl.GetPort() << std::endl
+						 << "Document: " << inputUrl.GetDocument() << std::endl;
+		}
+		catch (std::exception& e)
+		{
+			outputStream << e.what() << std::endl;
+		}
+	}
+
+	return;
+}
